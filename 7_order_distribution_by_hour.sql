94d1fed752a8d39ad940ac11b4b354157435299c
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2024-08-21 23:25:40 +0530

    query for the questions

---
+++ 7_order_distribution_by_hour.sql
@@ -0,0 +1,9 @@
+-- Determine the distribution of orders by hour of the day.
+
+SELECT
+    HOUR(orders.order_time) AS order_hour,
+    COUNT(orders.order_id)
+FROM
+    orders
+GROUP BY
+    order_hour;
---
+++ 9_avg_pizza_ordered_per_day.sql
@@ -0,0 +1,17 @@
+-- Group the orders by date and calculate the average number of pizzas ordered per day.
+
+WITH order_quantity AS (
+    SELECT
+        orders.order_date,
+        SUM(order_details.quantity) AS quantity
+    FROM
+        orders
+    INNER JOIN order_details ON orders.order_id = order_details.order_id
+    GROUP BY
+        orders.order_date
+)
+
+SELECT
+    ROUND(AVG(order_quantity.quantity),0) AS avg_pizza_ordered_per_day
+FROM
+    order_quantity;
